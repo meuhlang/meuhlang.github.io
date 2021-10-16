1d4246599fade30dacf8d3fb5b0e527272d7737f
Laurent Verdoïa <verdoialaurent@gmail.com>
2021-10-16 22:56:23 +0200

    Simplify GitHub super-Linter usage/configuration

--- scripts/super-linter.sh
+++ scripts/super-linter.sh
@@ -7,11 +7,7 @@ main() {
   cd "$(dirname "$0")/.."
 
   docker run \
-    --env FILTER_REGEX_EXCLUDE=site/docs/about/license.md \
     --env RUN_LOCAL=true \
-    --env VALIDATE_CSS=false \
-    --env VALIDATE_JAVASCRIPT_ES=false \
-    --env VALIDATE_JAVASCRIPT_STANDARD=false \
     --mount "type=bind,src=${PWD},dst=/tmp/lint" \
     github/super-linter:v4.8.1
 }
