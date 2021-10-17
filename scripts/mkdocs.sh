faef760eb498d11d331b3e1ad630f7963c559d99
Laurent VERDOÏA <verdoialaurent@gmail.com>
2021-10-17 22:27:15 +0200

    Fix local wrapper script of mkdocs

--- scripts/mkdocs.sh
+++ scripts/mkdocs.sh
@@ -6,12 +6,25 @@ set -o nounset
 main() {
   cd "$(dirname "$0")/.."
 
+  set -- lvjp/mkdocs:v1.2.3-3 mkdocs "$@"
+
+  if [ "${3:-}" = "serve" ]; then
+    os=$(docker version --format '{{.Client.Os}}')
+    case "${os}" in
+      darwin) set -- --publish 8000:8000 "$@" --dev-addr 0.0.0.0:8000;;
+      linux) set -- --network host "$@";;
+      *)
+        echo "Unsupported os: ${os}"
+        exit 1
+        ;;
+    esac
+  fi
+
   docker run \
     --mount "type=bind,src=${PWD}/site,dst=/work,ro" \
     --workdir /work \
-    --network host \
-    lvjp/mkdocs:v1.2.3-2 \
-    mkdocs "$@"
+    --rm \
+    "$@"
 }
 
 main "$@"
