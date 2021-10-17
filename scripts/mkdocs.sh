5119d0ca900ea2b4d387f5f778ccdd901cbd68cc
Laurent Verdoïa <verdoialaurent@gmail.com>
2021-10-17 09:17:20 +0200

    Use our publicly available mkdocs docker image

--- scripts/mkdocs.sh
+++ scripts/mkdocs.sh
@@ -6,20 +6,11 @@ set -o nounset
 main() {
   cd "$(dirname "$0")/.."
 
-  if command -v mkdocs > /dev/null 2>&1; then
-    cd site
-    exec mkdocs "$@"
-  fi
-
-  docker build \
-    --tag local/meuhlang.github.io/mkdocs:latest \
-    - < ./scripts/mkdocs.Dockerfile
-
   docker run \
     --mount "type=bind,src=${PWD}/site,dst=/work,ro" \
     --workdir /work \
     --network host \
-    local/meuhlang.github.io/mkdocs:latest \
+    lvjp/mkdocs:v1.2.3-2 \
     mkdocs "$@"
 }
 
